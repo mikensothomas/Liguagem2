813ca082872b63d2ee30abe3a3669ce861a20cf1
Mikenson Thomas <90985345+mikensothomas@users.noreply.github.com>
2022-07-02 02:44:43 -0300

    Add files via upload

---
+++ Atividade Extra.c
@@ -0,0 +1,30 @@
+                                                        /*
+                                                        MVC
+Neste tutorial vamos conhecer um pouco sobre o padrão MVC. 
+Model-view-controller (MVC) é um padrão de arquitetura de software. 
+Com o aumento da complexidade das aplicações desenvolvidas torna-se fundamental
+a separação entre os dados (Model) e o layout (View). 
+Desta forma, alterações feitas no layout não afectam a manipulação de dados, e 
+estes poderão ser reorganizados sem alterar o layout.
+
+                                                                 MVVM
+O MVVM é um pattern que foi criado em 2005, por John Gossman, 
+dos arquitetos do WPF e Silverlight na Microsoft. 
+O MVVM assemelha-se em alguns aspectos o MVC (Model View Controller) e ao MVP (Model View Presenter), 
+podemos até dizer que o MVVM é uma especialização do MVP adaptado para a arquitetura do WPF E Silverlight. 
+Conceitualmente, o MVVM e o MVP são idênticos, o que os diferencia é que o MVVM é específico
+para a arquitetura do WPF e Silverlight e o MVP é independente de plataforma. 
+O MVVM, visa estabelecer uma clara separação de responsabilidades em uma aplicação WPF e Silverlight, 
+mantendo uma espécie de façade entre o Modelo de objetos 
+( entenda classes de negócio, serviços externos e até mesmo acesso a banco de dados ) 
+e a View que é a interface, com a qual o usuário interage. 
+                                                                   
+                                                                  MVP
+MVP é a sigla de Minimum Viable Product e significa produto mínimo viável.
+É uma prática de administração de empresas que consiste em lançar um novo produto ou serviço com o menor investimento possível, 
+para testar o negócio antes de aportar grandes investimentos.
+                                           
+                                                                 Microserviços
+A arquitetura de microsserviços é uma abordagem na qual um único aplicativo é composto de muitos serviços
+menores que são implementáveis de forma independente e têm acoplamento fraco.
+*/
