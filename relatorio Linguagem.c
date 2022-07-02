0b1de1796a35abaa3bfc6f0c6aa825d588be6a94
Mikenson Thomas <90985345+mikensothomas@users.noreply.github.com>
2022-07-02 02:20:08 -0300

    Add files via upload

---
+++ relatorio Linguagem.c
@@ -0,0 +1,23 @@
+                                         /* MVC  
+
+  
+
+O MVC tem três partes, 
+Uma parte que é a Entrada, outra que é os Dados/Regras de negócio 
+E a outra que é a Saída.  
+
+A saída é feita através das visões(View).  
+
+A entrada é feita através dos controladores dos controles(Controller). 
+
+O Modelo (Model). 
+
+O controlador(Controller) recebe a entrada do usuário, pode ser um teclado, 
+pode ser um mouse qualquer tipo de entrada física de usuário, 
+ele vai tomar ações a partir dessa entrada normalmente a ação é mudar o estado do sistema
+um modo que a gente chama de modelo (Model), o modelo do sistema vai conter os dados, 
+o usuário vai usar, o controlador vai usar esses dispositivos que vão mandar uma mensagem para o 
+controlador que vão mudar o modelo. 
+Quando mudar o modelo e mudar o estado do sistema tipicamente a gente vai precisar atualizar
+a visão que dá a interface gráfica. 
+E daí a gente vai ter esse componente chamado  View que é quem responsável por atualizar a visão para fora. */
